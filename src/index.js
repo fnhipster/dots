c11cabe1c637a808369de9222f29db401f82cd33
Carlos A. Cabrera <carlosacabrera@users.noreply.github.com>
2018-01-29 13:25:55 -0600

    Update index.js
    removing leftover code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import '../public/main.css';
 
-import { createStore } from './lib/element';
-
 import { GameComponent } from './components/Game/Game.component';
 
 /** Add application to the DOM */
